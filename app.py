495763af9e0de23701aae02c3ffbde353d70aef8
terminatorash2199 <aashalkamdar@gmail.com>
2020-06-19 00:49:58 +0530

    basic fake note prediction apiwith csv and normal

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def predict_note_authentication():
 	entropy = request.args.get('entropy')
 	prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
 
-	return "the predicted value is " + str(prediction)
-	
+	return "The predicted value  is " + str(prediction)
+
 
 @app.route('/predict_file',methods=['POST'])
 def predict_note_file():
